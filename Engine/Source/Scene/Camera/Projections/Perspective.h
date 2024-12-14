deb465d5102f90a8e28d47098dd2bbf096185fe8
vicenteconejerosdelacruz <vicenteconejeros@gmail.com>
2024-12-14 16:30:10 -0300

    adding nearZ to perspective projection

--- Engine/Source/Scene/Camera/Projections/Perspective.h
+++ Engine/Source/Scene/Camera/Projections/Perspective.h
@@ -8,8 +8,10 @@ namespace Scene::Camera::Projections {
 
 	struct Perspective {
 		static constexpr float defaultFovAngleY = (70.0f * XM_PI / 180.0f);
+		static constexpr float defaultNearZ = 0.01f;
 		static constexpr float defaultFarZ = 100.0f;
 
+		float nearZ = defaultNearZ;
 		float farZ = defaultFarZ;
 		float fovAngleY = defaultFovAngleY;
 		UINT width = 0;
@@ -21,7 +23,7 @@ namespace Scene::Camera::Projections {
 			this->width = width;
 			this->height = height;
 			float aspectRatio = static_cast<float>(width) / static_cast<float>(height);
-			projectionMatrix = XMMatrixPerspectiveFovRH(fovAngleY, aspectRatio, 0.01f, farZ);
+			projectionMatrix = XMMatrixPerspectiveFovRH(fovAngleY, aspectRatio, nearZ, farZ);
 
 		};
 
