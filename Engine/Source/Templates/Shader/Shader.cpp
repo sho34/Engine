a73401af71237f51359fdbfbb90579af4a4d14fc
vicenteconejerosdelacruz <vicenteconejeros@gmail.com>
2025-03-01 00:33:48 -0300

    cleanup

--- Engine/Source/Templates/Shader/Shader.cpp
+++ Engine/Source/Templates/Shader/Shader.cpp
@@ -5,57 +5,9 @@
 
 namespace Templates {
 
-	//std::map<std::string, std::shared_ptr<Shader>> shaderTemplates;
 	std::map<std::string, nlohmann::json> shaderTemplates;
-	/*
-	std::map<ShaderType, std::string> ShaderT::shaderEntryPoint = {
-		 {	ShaderType::VERTEX_SHADER, "main_vs"	},
-		 {	ShaderType::PIXEL_SHADER, "main_ps"	},
-	};
-	std::map<ShaderType, std::string> ShaderT::shaderTarget = {
-		 {	ShaderType::VERTEX_SHADER, "vs_6_1"	},
-		 {	ShaderType::PIXEL_SHADER, "ps_6_1"},
-	};
-	*/
-	//TemplatesNotification<ShaderPtr*> shaderChangeNotifications;
 
 	//NOTIFICATIONS
-	/*
-	static void OnShaderCompilationStart(void* shaderPtr) {
-		auto shader = static_cast<ShaderPtr*>(shaderPtr);
-		if (shaderChangeNotifications.contains(shader)) {
-			NotifyOnLoadStart<ShaderPtr*>(shaderChangeNotifications[shader]);
-		}
-		(*shader)->flags|= TemplateFlags::Loading;
-		(*shader)->pixelShader = nullptr;
-		(*shader)->vertexShader = nullptr;
-	}
-	*/
-
-	/*
-	static void OnShaderCompilationComplete(void* shaderPtr, void* shaderCompilerOutputPtr) {
-		auto shader = static_cast<ShaderPtr*>(shaderPtr);
-		auto output = static_cast<ShaderCompilerOutputPtr*>(shaderCompilerOutputPtr);
-		switch ((*output)->shaderType) {
-		case ShaderType::VERTEX_SHADER:
-			(*shader)->vertexShader = *output;
-			break;
-		case ShaderType::PIXEL_SHADER:
-			(*shader)->pixelShader = *output;
-			break;
-		}
-		if ((*shader)->vertexShader && (*shader)->pixelShader != nullptr) {
-			(*shader)->loading = false;
-			if (shaderChangeNotifications.contains(shader)) {
-				NotifyOnLoadComplete(shader,shaderChangeNotifications[shader]);
-			}
-		}
-	}
-	*/
-
-	/*
-	static void OnShaderCompilerOutputDestroy(void* shaderPtr, void* shaderCompilerOutputPtr) {}
-	*/
 
 	//CREATE
 	void CreateShader(std::string name, nlohmann::json json)
