9efc38eab77a983fb094992f8197263cd42863dc
vicenteconejerosdelacruz <vicenteconejeros@gmail.com>
2025-03-21 01:18:54 -0300

    adding default3DModelsFolder

--- Engine/Source/pch/Application.h
+++ Engine/Source/pch/Application.h
@@ -4,3 +4,4 @@ static const std::string defaultLevelsFolder = "Levels/";
 static const std::string defaultTemplatesFolder = "Templates/";
 static const std::string defaultShadersFolder = "Shaders/";
 static const std::string defaultAssetsFolder = "Assets/";
+static const std::string default3DModelsFolder = "Assets/models/";
